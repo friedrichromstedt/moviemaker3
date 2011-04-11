1509940c0cd00e46ffcb891a45edaffd99a9dca4
Friedrich Romstedt <friedrichromstedt@gmail.com>
2011-04-11 21:50:34 +0200

    Removing deprecated cruft

--- moviemaker3/__init__.py
+++ moviemaker3/__init__.py
@@ -1,6 +1,3 @@
-from moviemaker3.function import *
-import moviemaker3.math
-from moviemaker3.math import *
 from moviemaker3.stacks import *
 from moviemaker3.parameter import *
 from moviemaker3.extension import *
